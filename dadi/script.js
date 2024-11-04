ae3577cc8620676db9b04117a145d81444dccbf9
Simone-Fratini <simone@fratini.com>
2024-11-04 16:21:48 +0100

    dado done with function

--- dadi/script.js
+++ dadi/script.js
@@ -1,1 +1,20 @@
 "use strict";
+
+
+function dado() {
+    return Math.random() * (6 - 1) + 1;
+}
+  
+const punteggioGiocatore = dado();
+const punteggioComputer = dado();
+
+console.log("numero giocatore:", punteggioGiocatore);
+console.log("numero computer:", punteggioComputer);
+
+if (punteggioGiocatore > punteggioComputer) {
+    alert("Il giocatore vince!");
+} else if (punteggioGiocatore < punteggioComputer) {
+    alert("Il computer vince!");
+} else {
+  alert("È un pareggio!");
+}
